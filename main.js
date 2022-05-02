93dabce70b7244841a14407812e367c9bcdb4d16
The_Girl_With_Codes <99324777+Olive-Moitra@users.noreply.github.com>
2022-05-02 03:43:42 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+console.log("ml5 version", ml5.version);
+
+Webcam.set(
+    {
+        width:350,
+        height:300,
+        image_format:"png",
+        png_quality:90
+    }
+);
+camera=document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_snapshot()
+{
+    Webcam.snap(
+        function(data_uri)
+        {
+            document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+        }
+    )
+}
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/t6jdUcZDO/model.json',modelloaded);
+function modelloaded()
+{
+    console.log("model loaded");
+}
+function check()
+{
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
